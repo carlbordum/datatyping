fc7cc8497f68482c22c064c26bfea596d130e9b0
Carl Bordum Hansen <carl@bordum.dk>
2017-07-26 18:52:34 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='datatyping',
+    description='Safe data validation for greater documentation and maintainability',
+    keywords='safe data validation typing type check',
+    url='https://github.com/Zaab1t/datatyping',
+    author='Carl Bordum Hansen',
+    author_email='carl@bordum.dk',
+    version='0.0.1',
+    copyright='Copyright (c) 2017 Carl Bordum Hansen',
+    license='MIT',
+    py_modules=['datatyping'],
+)
