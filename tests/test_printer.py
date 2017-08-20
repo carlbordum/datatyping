0ba8fe3acd260da67599f9ad7d87d2704401066a
Carl Bordum Hansen <carl@bordum.dk>
2017-08-20 14:29:06 +0200

    advanced test

--- tests/test_printer.py
+++ tests/test_printer.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 from datatyping.printer import pformat
 
 
@@ -20,3 +21,21 @@ def test_simple_dict():
 
 def test_dict_mix():
     assert pformat({'a': [1, 2, 3], 'b': 'c'}) == "{'a': [int], 'b': str}"
+
+
+def test_advanced():
+    expected = ("{   'args': dict,"
+    "\n    'data': str,"
+    "\n    'files': dict,"
+    "\n    'form': dict,"
+    "\n    'headers': {'Accept': str,"
+    "\n                'Accept-Encoding': str,"
+    "\n                'Connection': str,"
+    "\n                'Host': 'httpbin.org',"
+    "\n                'User-Agent': str},"
+    "\n    'json': NoneType,"
+    "\n    'method': str,"
+    "\n    'origin': str,"
+    "\n    'url': str}")
+    r = requests.get('http://httpbin.org/anything')
+    assert pformat(r.json()) == expected
