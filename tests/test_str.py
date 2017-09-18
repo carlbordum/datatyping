2da4d4ec77983a68930ec81f9c83df9551312b96
Carl Bordum Hansen <carl@bordum.dk>
2017-09-18 16:35:17 +0200

    added a few string tests

--- tests/test_str.py
+++ tests/test_str.py
@@ -3,5 +3,13 @@ from datatyping import validate
 
 
 def test_simple():
+    assert validate([str], ['ab', 'cd']) is None
+
+
+def test_simple_error():
     with pytest.raises(TypeError):
         validate([str], 'asd')
+
+
+def test_nested():
+    assert validate([[str]], [['ab', 'cd'], ['ef', 'g']]) is None
