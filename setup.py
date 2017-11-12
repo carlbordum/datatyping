cc60d0838186fd6be2975e07fdcf21c9016397d6
Carl Bordum Hansen <carl@bordum.dk>
2017-11-12 19:08:19 +0100

    pypi friendly (valid email, long_description)

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as readme:
+    long_description = '\n' + readme.read()
+
 setup(
     name='datatyping',
     description='Pythonic type checking',
+    long_description=long_description,
     keywords='safe data validation typing type check',
     url='https://github.com/Zaab1t/datatyping',
     author='Carl Bordum Hansen',
-    author_email='carl -AT- bordum.dk',
-    version='0.5.0',
+    author_email='carl@bordum.dk',
+    version='0.5.1',
     copyright='Copyright (c) 2017 Carl Bordum Hansen',
     license='MIT',
     packages=['datatyping'],
