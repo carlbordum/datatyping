9d37ea92ab417d7af7021a8eefd678f19af14b5d
Michaël Lévesque-Dion <michael.levesque-dion.1@ulaval.ca>
2018-10-03 22:12:39 -0400

    Add hypothesis tests for lists

--- tests/test_list_struct.py
+++ tests/test_list_struct.py
@@ -1,4 +1,7 @@
 import pytest
+from hypothesis import given
+from hypothesis.strategies import lists, integers, floats, text, one_of, dictionaries
+
 from datatyping import validate
 
 
@@ -6,13 +9,15 @@ def test_empty():
     assert validate([], []) is None
 
 
-def test_plain():
-    assert validate([int], [1, 2, 3, 4, 5]) is None
+@given(li=lists(integers()))
+def test_plain(li):
+    assert validate([int], li) is None
 
 
-def test_plain_typeerror():
+@given(lst=lists(one_of(floats(), text()), min_size=1))
+def test_plain_typeerror(lst):
     with pytest.raises(TypeError):
-        validate([int], [1, 2, 3, 4.5])
+        validate([int], lst)
 
 
 def test_list_lengths():
@@ -20,27 +25,35 @@ def test_list_lengths():
         validate([int, int, int, str], [1, 2, 3, 'a', 'a'])
 
 
-def test_list_lengths_same_type():
+@given(li=lists(integers(), max_size=1))
+def test_list_too_short_same_type(li):
     with pytest.raises(ValueError):
-        validate([int, int], [1])
+        validate([int, int], li)
 
 
-def test_list_lengths_same_type_2():
+@given(li=lists(integers(), min_size=3))
+def test_list_too_long_same_type(li):
     with pytest.raises(ValueError):
-        validate([int, int], [1, 2, 2])
+        validate([int, int], li)
 
 
 def test_dict_empty():
     assert validate([dict], [{}, {}, {}]) is None
 
 
-def test_dict_strict():
-    assert validate([{'a': int}], [{'a': 123}, {'a': 456}]) is None
+@given(i=integers())
+def test_dict_strict(i):
+    assert validate([{'a': int}], [{'a': i}, {'a': 456}]) is None
+
+
+@given(lists(dictionaries(one_of(integers(), text()), one_of(integers(), text()))))
+def test_any_dict(d):
+    assert validate([dict], d) is None
 
 
 def test_dict_nested():
     assert validate([{'a': {'b': [dict]}}],
-        [
-            {'a': {'b': [{}, {}]}},
-            {'a': {'b': [{'any': 'key'}, {'used': 'here'}]}},
-        ]) is None
+                    [
+                        {'a': {'b': [{}, {}]}},
+                        {'a': {'b': [{'any': 'key'}, {'used': 'here'}]}},
+                    ]) is None
