75368e685e4bbad8f48e17dbcea1882794d7c3e4
illright <leva181777@gmail.com>
2018-10-13 15:41:55 +0300

    Rewrite list_struct tests with hypothesis

--- tests/test_list_struct.py
+++ tests/test_list_struct.py
@@ -1,6 +1,7 @@
 import pytest
 from hypothesis import given
-from hypothesis.strategies import lists, integers, floats, text, one_of, dictionaries
+from hypothesis.strategies import lists, integers, floats, text, one_of, dictionaries, \
+    fixed_dictionaries
 
 from datatyping.datatyping import validate
 
@@ -14,36 +15,27 @@ def test_plain(li):
     assert validate([int], li) is None
 
 
-@given(lst=lists(one_of(floats(), text()), min_size=1))
-def test_plain_typeerror(lst):
+@given(lst=lists(floats(), min_size=1))
+def test_plain_type_error(lst):
     with pytest.raises(TypeError):
         validate([int], lst)
 
 
-def test_list_lengths():
+@given(lst=one_of(lists(integers(), min_size=5),
+                  lists(integers(), max_size=3)))
+def test_list_lengths(lst):
     with pytest.raises(ValueError):
-        validate([int, int, int, str], [1, 2, 3, 'a', 'a'])
+        validate([int, int, int, str], lst)
 
 
-@given(li=lists(integers(), max_size=1))
-def test_list_too_short_same_type(li):
-    with pytest.raises(ValueError):
-        validate([int, int], li)
+@given(lst=lists(dictionaries(max_size=0)))
+def test_dict_empty(lst):
+    assert validate([dict], lst) is None
 
 
-@given(li=lists(integers(), min_size=3))
-def test_list_too_long_same_type(li):
-    with pytest.raises(ValueError):
-        validate([int, int], li)
-
-
-def test_dict_empty():
-    assert validate([dict], [{}, {}, {}]) is None
-
-
-@given(i=integers())
-def test_dict_strict(i):
-    assert validate([{'a': int}], [{'a': i}, {'a': 456}]) is None
+@given(lst=lists(fixed_dictionaries({'a': integers()})))
+def test_dict_strict(lst):
+    assert validate([{'a': int}], lst) is None
 
 
 @given(lists(dictionaries(one_of(integers(), text()), one_of(integers(), text()))))
