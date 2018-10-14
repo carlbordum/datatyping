c092a9a836046aa098b8ac10f63bc60f70da9e76
illright <leva181777@gmail.com>
2018-10-14 12:03:32 +0300

    Improve the str tests by adding bad types

--- tests/test_str.py
+++ tests/test_str.py
@@ -1,6 +1,6 @@
 import pytest
 from hypothesis import given
-from hypothesis.strategies import integers, text
+from hypothesis.strategies import integers, text, lists, iterables, one_of
 
 from datatyping.datatyping import validate
 
@@ -10,7 +10,7 @@ def test_simple(string):
     assert validate(str, string) is None
 
 
-@given(not_string=integers())
+@given(not_string=one_of(integers(), lists(integers()), iterables(integers())))
 def test_simple_error(not_string):
     with pytest.raises(TypeError):
         validate(str, not_string)
